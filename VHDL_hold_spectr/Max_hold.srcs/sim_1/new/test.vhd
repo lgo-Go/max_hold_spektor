fa7c52f8e84b346d99773d5cdbda42fb1880b088
Igor <pupokbomzha@yandex.ru>
2019-12-25 01:16:02 +0300

    отформатирован код topfft и tb

--- VHDL_hold_spectr/Max_hold.srcs/sim_1/new/test.vhd
+++ VHDL_hold_spectr/Max_hold.srcs/sim_1/new/test.vhd
@@ -1,147 +1,143 @@
-LIBRARY ieee; 
-USE ieee.std_logic_1164.all;
- USE ieee.std_logic_arith.all;
- 
-ENTITY tb IS 
-END ENTITY tb; 
-
-ARCHITECTURE a OF tb IS 
-
-signal tbdin : STD_LOGIC_VECTOR (31 downto 0); 
-signal tbout : STD_LOGIC_VECTOR (31 downto 0); 
-signal tboutIm : STD_LOGIC_VECTOR (15 downto 0);
-signal tboutRe : STD_LOGIC_VECTOR (15 downto 0);  
-signal tbclk : std_logic; 
-signal tbrst : std_logic:='0'; 
-signal tbrfd : std_logic:='1'; 
-signal tbdv : std_logic:='1'; 
-signal tbsign : std_logic:='1'; 
-signal s_axis_data_tvalid : std_logic:='0'; 
-signal s_axis_data_tready : std_logic:='1'; 
-signal options: std_logic_vector(3 downto 0):=(others=> '0');
-signal tbshifter:std_logic_vector(8 downto 0):="000000000";
-signal tbconditions:std_logic_vector(15 downto 0):="0000000000000000";
-signal    event_frame_started :STD_LOGIC;
-signal    event_tlast_unexpected :  STD_LOGIC;
-signal    event_tlast_missing : STD_LOGIC;
-signal    event_status_channel_halt : STD_LOGIC;
-signal    event_data_in_channel_halt : STD_LOGIC;
-signal    event_data_out_channel_halt : STD_LOGIC;
-
-
-component ReadFull is 
---generic( 
---numOfBits : integer:=16;
---file_name : string 
- Port (
-data: out STD_LOGIC_VECTOR (31 downto 0); 
-dv: out std_logic; 
-rst : in std_logic; 
-rfd : in std_logic;
-clk : in std_logic);  
-end component; 
-
-component topfft is
-port( 
-data_in: in std_logic_vector(31 downto 0); 
-clk: in std_logic; 
-dv: in std_logic;
-options: in std_logic_vector(3 downto 0);
-shifter: in std_logic_vector(9 downto 0);
-s_axis_data_tvalid :in std_logic:='0'; 
-s_axis_data_tready : out std_logic:='0'; 
-conditions: out std_logic_vector(15 downto 0);
-tvalid: in std_logic:='1'; 
-data_out: out std_logic_vector(31 downto 0)); 
-end component;
-
-component WriteData IS 
-generic( 
-numOfBits: integer:=16;
-file_name : string := "D:\÷”»Ãœ  ”–—¿◊\max_hold_spektor\matlab_function\DataFromfpgaI.dat"); 
-port( 
-clk,dv,sign : in std_logic; 
-DataIn : in std_logic_vector ((numOfBits-1) downto 0)); 
-end component; 
-
-
-
-BEGIN 
-
-
-
-generate_clk : process begin 
-loop 
-tbclk <= '1'; 
-wait for 25 ns; 
-tbclk <= '0'; 
-wait for 25 ns; 
-end loop; 
-end process; 
-
-
-generate_valid:process (tbclk) begin 
-    if rising_edge(tbclk) then
-        if tbshifter="111111111" then
-    options<="1100";  
-     else         
-tbshifter<=unsigned(tbshifter) + 1; 
-options <= "1010";
-
-          
- end if;
-end if;
-end process; 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
  
 
-core: topfft
-port map( 
-options=>options,
-data_in=>tbdin, 
-clk=>tbclk,
-dv=>tbdv,
- --s_axis_data_tvalid =>s_axis_data_tvalid ,
- --s_axis_data_tready => s_axis_data_tready,
-conditions=>tbconditions,
-shifter=>tbshifter,
-tvalid=>tbdv,
-data_out=>tbout); 
-
-read : ReadFull 
-port map ( 
-dv => tbdv, 
-clk => tbclk, 
-rfd => tbrfd, 
-rst => tbrst, 
-data => tbdin); 
+entity tb is
+end entity tb;
 
 
+architecture a of tb is
 
-tboutIm<=tbout(31 downto 16);
-tboutRe<=tbout(15 downto 0);
-write : WriteData
-generic map( 
-        numOfBits => 16,
-        file_name => "D:\÷”»Ãœ  ”–—¿◊\max_hold_spektor\matlab_function\DataFromfpgaI.dat")
-port map ( 
-clk => tbclk, 
-dv => tbdv, 
-sign => tbsign, 
-DataIn => tboutIm); 
+    signal tbdin : std_logic_vector (31 downto 0);
+    signal tbout : std_logic_vector (31 downto 0);
+    signal tboutim : std_logic_vector (15 downto 0);
+    signal tboutre : std_logic_vector (15 downto 0);
+    signal tbclk : std_logic;
+    signal tbrst : std_logic := '0';
+    signal tbrfd : std_logic := '1';
+    signal tbdv : std_logic := '1';
+    signal tbsign : std_logic := '1';
+    signal s_axis_data_tvalid : std_logic := '0';
+    signal s_axis_data_tready : std_logic := '1';
+    signal options : std_logic_vector(3 downto 0) := (others=> '0');
+    signal tbshifter : std_logic_vector(8 downto 0) := "000000000";
+    signal tbconditions : std_logic_vector(15 downto 0) := "0000000000000000";
+    signal event_frame_started : std_logic;
+    signal event_tlast_unexpected :  std_logic;
+    signal event_tlast_missing : std_logic;
+    signal event_status_channel_halt : std_logic;
+    signal event_data_in_channel_halt : std_logic;
+    signal event_data_out_channel_halt : std_logic;
 
-write2 : WriteData
-generic map( 
-        numOfBits => 16,
-        file_name => "D:\÷”»Ãœ  ”–—¿◊\max_hold_spektor\matlab_function\DataFromfpgaQ.dat")
-port map ( 
-clk => tbclk, 
-dv => tbdv, 
-sign => tbsign, 
-DataIn => tboutRe); 
-    event_frame_started<=tbconditions(4);
-    event_tlast_unexpected<=tbconditions(5);
-    event_tlast_missing<=tbconditions(6);
-    event_status_channel_halt<=tbconditions(7);
-    event_data_in_channel_halt<=tbconditions(8);
-    event_data_out_channel_halt<=tbconditions(9);
-END a;
+
+    component readfull is
+    --generic( 
+    --numofbits : integer:=16;
+    --file_name : string 
+        port(
+            data : out std_logic_vector (31 downto 0);
+            dv : out std_logic;
+            rst : in std_logic;
+            rfd : in std_logic
+            clk : in std_logic);
+    end component;
+
+    component topfft is
+        port(
+            data_in : in std_logic_vector(31 downto 0);
+            clk : in std_logic;
+            dv : in std_logic;
+            options : in std_logic_vector(3 downto 0);
+            shifter : in std_logic_vector(9 downto 0);
+            s_axis_data_tvalid : in std_logic := '0';
+            s_axis_data_tready : out std_logic := '0';
+            conditions : out std_logic_vector(15 downto 0);
+            tvalid : in std_logic := '1';
+            data_out : out std_logic_vector(31 downto 0));
+    end component;
+
+    component writedata is
+        generic(
+            numofbits : integer := 16;
+            file_name : string := "d:\ˆÛËÏÔ ÍÛÒ‡˜\max_hold_spektor\matlab_function\datafromfpgai.dat");
+        port( 
+            clk, dv, sign : in std_logic;
+            datain : in std_logic_vector ((numofbits-1) downto 0));
+    end component;
+
+begin
+
+    generate_clk : process begin
+        loop
+            tbclk <= '1';
+            wait for 25 ns;
+            tbclk <= '0';
+            wait for 25 ns;
+        end loop;
+    end process;
+
+
+    generate_valid:process (tbclk) begin
+        if rising_edge(tbclk) then
+            if tbshifter = "111111111" then
+                options <= "1100";  
+            else
+                tbshifter <= unsigned(tbshifter) + 1;
+                options <= "1010";
+            end if;
+        end if;
+    end process;
+
+    core: topfft
+    port map(
+        options => options,
+        data_in => tbdin,
+        clk => tbclk,
+        dv => tbdv,
+         --s_axis_data_tvalid =>s_axis_data_tvalid ,
+         --s_axis_data_tready => s_axis_data_tready,
+        conditions => tbconditions,
+        shifter => tbshifter,
+        tvalid => tbdv,
+        data_out => tbout);
+
+    read : readfull
+    port map (
+        dv => tbdv,
+        clk => tbclk,
+        rfd => tbrfd,
+        rst => tbrst,
+        data => tbdin);
+
+    tboutim <= tbout(31 downto 16);
+    tboutre <= tbout(15 downto 0);
+
+    write : writedata
+    generic map(
+            numofbits => 16,
+            file_name => "d:\ˆÛËÏÔ ÍÛÒ‡˜\max_hold_spektor\matlab_function\datafromfpgai.dat")
+    port map (
+        clk => tbclk,
+        dv => tbdv,
+        sign => tbsign,
+        datain => tboutim);
+
+    write2 : writedata
+    generic map(
+            numofbits => 16,
+            file_name => "d:\ˆÛËÏÔ ÍÛÒ‡˜\max_hold_spektor\matlab_function\datafromfpgaq.dat")
+    port map (
+        clk => tbclk,
+        dv => tbdv,
+        sign => tbsign,
+        datain => tboutre);
+
+    event_frame_started <= tbconditions(4);
+    event_tlast_unexpected <= tbconditions(5);
+    event_tlast_missing <= tbconditions(6);
+    event_status_channel_halt <= tbconditions(7);
+    event_data_in_channel_halt <= tbconditions(8);
+    event_data_out_channel_halt <= tbconditions(9);
+
+end a;
--- VHDL_hold_spectr/Max_hold.srcs/sources_1/new/topfft.vhd
+++ VHDL_hold_spectr/Max_hold.srcs/sources_1/new/topfft.vhd
@@ -1,116 +1,121 @@
-library IEEE; 
-use IEEE.STD_LOGIC_1164.ALL;
-use ieee.std_logic_arith.all; 
-entity topfft is 
-port( 
-data_in: in std_logic_vector(31 downto 0); 
-clk: in std_logic; 
-dv: in std_logic; 
-s_axis_data_tvalid :in std_logic:='0'; 
-s_axis_data_tready : out std_logic:='0'; 
-options: in std_logic_vector(3 downto 0);
-shifter: in std_logic_vector(8 downto 0);
-tvalid: in std_logic:='1'; 
-data_out: out std_logic_vector(31 downto 0); 
-conditions: out std_logic_vector(15 downto 0)); 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
 
+
+entity topfft is 
+    port(
+        data_in : in std_logic_vector(31 downto 0);
+        clk : in std_logic;
+        dv : in std_logic;
+        s_axis_data_tvalid : in std_logic := '0';
+        s_axis_data_tready : out std_logic := '0';
+        options : in std_logic_vector(3 downto 0);
+        shifter : in std_logic_vector(8 downto 0);
+        tvalid : in std_logic := '1';
+        data_out : out std_logic_vector(31 downto 0);
+        conditions : out std_logic_vector(15 downto 0));
 end topfft; 
 
-architecture Behavioral of topfft is 
-type sample is array(511 downto 0) of std_logic_vector (31 downto 0);
-signal ish: sample;
-signal sr: sample;
-signal cntr: std_logic_vector(9 downto 0);
-signal last : std_logic:='0';
-signal buffer_in : std_logic_vector(31 downto 0);
-signal buffer_out : std_logic_vector(31 downto 0);
-signal config : std_logic_vector(15 downto 0):=("0000000000000000");
-signal cond: std_logic_vector(15 downto 0):=("0000000000000000");
-component xfft_0 is 
-PORT (
-    aclk : IN STD_LOGIC:='0';
-    s_axis_config_tdata : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-    s_axis_config_tvalid : IN STD_LOGIC;
-    s_axis_config_tready : OUT STD_LOGIC;
-    s_axis_data_tdata : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-    s_axis_data_tvalid : IN STD_LOGIC;
-    s_axis_data_tready : OUT STD_LOGIC;
-    s_axis_data_tlast : IN STD_LOGIC;
-    m_axis_data_tdata : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-    m_axis_data_tvalid : OUT STD_LOGIC;
-    m_axis_data_tready : IN STD_LOGIC:='1';
-    m_axis_data_tlast : OUT STD_LOGIC;
-    event_frame_started : OUT STD_LOGIC;
-    event_tlast_unexpected : OUT STD_LOGIC;
-    event_tlast_missing : OUT STD_LOGIC;
-    event_status_channel_halt : OUT STD_LOGIC;
-    event_data_in_channel_halt : OUT STD_LOGIC;
-    event_data_out_channel_halt : OUT STD_LOGIC
-  );
-end component; 
-begin 
---begin 
---process(clk) begin 
---if rising_edge(clk) then 
---    if cntr = "111111111" then
---    cntr<="111111111"; 
---      buffer_in <= (others => '0');
---      else cntr <= unsigned(cntr) + 1;
-buffer_in<=data_in;
---end if;
---end if;
---end process;
 
-fft:xfft_0
-port map(  
-    aclk=>clk,
-      s_axis_config_tdata=>config,
-      s_axis_config_tvalid=>'0',
-    s_axis_config_tready=>open,
-      s_axis_data_tdata=>buffer_in,
-      s_axis_data_tvalid=>options(1),
-    s_axis_data_tready =>s_axis_data_tready,
-      s_axis_data_tlast=>options(2),
-      m_axis_data_tdata=>buffer_out,
-    m_axis_data_tvalid=>cond(2),
-      m_axis_data_tready=>options(3),
-    m_axis_data_tlast=>cond(3),
-    event_frame_started=>cond(4),
-    event_tlast_unexpected=>cond(5),
-    event_tlast_missing=>cond(6),
-    event_status_channel_halt=>cond(7),
-    event_data_in_channel_halt=>cond(8),
-    event_data_out_channel_halt=>cond(9)
-);
---fft:xfft_0
---port map( 
---    aclk=>clk,
---      s_axis_config_tdata=>config,
---      s_axis_config_tvalid=>options(0),
---    s_axis_config_tready=>cond(0),
---      s_axis_data_tdata=>buffer_in,
---      s_axis_data_tvalid=>s_axis_data_tvalid,
---    s_axis_data_tready =>s_axis_data_tready,
---      s_axis_data_tlast=>options(2),
---      m_axis_data_tdata=>buffer_out,
---    m_axis_data_tvalid=>cond(2),
---      m_axis_data_tready=>options(3),
---    m_axis_data_tlast=>cond(3),
---    event_frame_started=>cond(4),
---    event_tlast_unexpected=>cond(5),
---    event_tlast_missing=>cond(6),
---    event_status_channel_halt=>cond(7),
---    event_data_in_channel_halt=>cond(8),
---    event_data_out_channel_halt=>cond(9)
---);
+architecture behavioral of topfft is
 
---srav: for i in 0 to 511 generate
---sam: sravnenie
---port map(ish(i)<= buffer_out,хз блять как сравнить тут эту хуету
+    type sample is array(511 downto 0) of std_logic_vector (31 downto 0);
+    signal ish : sample;
+    signal sr : sample;
+
+    signal cntr : std_logic_vector(9 downto 0);
+    signal last : std_logic := '0';
+    signal buffer_in : std_logic_vector(31 downto 0);
+    signal buffer_out : std_logic_vector(31 downto 0);
+    signal config : std_logic_vector(15 downto 0) := ("0000000000000000");
+    signal cond : std_logic_vector(15 downto 0) := ("0000000000000000");
+
+    component xfft_0 is
+        port(
+            aclk : in std_logic := '0';
+            s_axis_config_tdata : in std_logic_vector(15 downto 0);
+            s_axis_config_tvalid : in std_logic;
+            s_axis_config_tready : out std_logic;
+            s_axis_data_tdata : in std_logic_vector(31 downto 0);
+            s_axis_data_tvalid : in std_logic;
+            s_axis_data_tready : out std_logic;
+            s_axis_data_tlast : in std_logic;
+            m_axis_data_tdata : out std_logic_vector(31 downto 0);
+            m_axis_data_tvalid : out std_logic;
+            m_axis_data_tready : in std_logic := '1';
+            m_axis_data_tlast : out std_logic;
+            event_frame_started : out std_logic;
+            event_tlast_unexpected : out std_logic;
+            event_tlast_missing : out std_logic;
+            event_status_channel_halt : out std_logic;
+            event_data_in_channel_halt : out std_logic;
+            event_data_out_channel_halt : out std_logic);
+    end component;
+
+    begin
+    --begin 
+    --process(clk) begin 
+    --if rising_edge(clk) then 
+    --    if cntr = "111111111" then
+    --    cntr<="111111111"; 
+    --      buffer_in <= (others => '0');
+    --      else cntr <= unsigned(cntr) + 1;
+    buffer_in<=data_in;
+    --end if;
+    --end if;
+    --end process;
+
+    fft:xfft_0
+    port map(  
+        aclk => clk,
+        s_axis_config_tdata => config,
+        s_axis_config_tvalid => '0',
+        s_axis_config_tready => open,
+        s_axis_data_tdata => buffer_in,
+        s_axis_data_tvalid => options(1),
+        s_axis_data_tready => s_axis_data_tready,
+        s_axis_data_tlast => options(2),
+        m_axis_data_tdata => buffer_out,
+        m_axis_data_tvalid => cond(2),
+        m_axis_data_tready => options(3),
+        m_axis_data_tlast => cond(3),
+        event_frame_started => cond(4),
+        event_tlast_unexpected => cond(5),
+        event_tlast_missing => cond(6),
+        event_status_channel_halt => cond(7),
+        event_data_in_channel_halt => cond(8),
+        event_data_out_channel_halt => cond(9));
+
+    --fft:xfft_0
+    --port map( 
+    --    aclk=>clk,
+    --      s_axis_config_tdata=>config,
+    --      s_axis_config_tvalid=>options(0),
+    --    s_axis_config_tready=>cond(0),
+    --      s_axis_data_tdata=>buffer_in,
+    --      s_axis_data_tvalid=>s_axis_data_tvalid,
+    --    s_axis_data_tready =>s_axis_data_tready,
+    --      s_axis_data_tlast=>options(2),
+    --      m_axis_data_tdata=>buffer_out,
+    --    m_axis_data_tvalid=>cond(2),
+    --      m_axis_data_tready=>options(3),
+    --    m_axis_data_tlast=>cond(3),
+    --    event_frame_started=>cond(4),
+    --    event_tlast_unexpected=>cond(5),
+    --    event_tlast_missing=>cond(6),
+    --    event_status_channel_halt=>cond(7),
+    --    event_data_in_channel_halt=>cond(8),
+    --    event_data_out_channel_halt=>cond(9)
+    --);
+
+    --srav: for i in 0 to 511 generate
+    --sam: sravnenie
+    --port map(ish(i)<= buffer_out,хз блять как сравнить тут эту хуету
 
 
 
-data_out<=buffer_out;
-conditions<=cond;
+    data_out <= buffer_out;
+    conditions <= cond;
 
-end Behavioral;
+end behavioral;
